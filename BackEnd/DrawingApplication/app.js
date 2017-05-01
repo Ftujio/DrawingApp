4225bd957f2a78b3bfb3bebfbde0f420ad4c8058
Kalin <thegroup1516@gmail.com>
2017-05-01 13:10:28 +0300

    Add the whole functionals in the app.js

--- BackEnd/DrawingApplication/app.js
+++ BackEnd/DrawingApplication/app.js
@@ -12,11 +12,13 @@ var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
-
+mongoose.connect('mongodb://localhost/drawingapp');
+var db = mongoose.connection;
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+// Init App
 var app = express();
 
 // view engine setup
@@ -30,6 +32,47 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Express session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+    errorFormatter: function (param, msg, value) {
+        var namespace = param.split('.')
+            , root = namespace.shift()
+            , formParam = root;
+
+        while (namespace.length) {
+          formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+          param: formParam,
+            msg: msg,
+            value: value
+        };
+    }
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Variables
+app.use(function (req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
+    next();
+});
 
 app.use('/', index);
 app.use('/users', users);
