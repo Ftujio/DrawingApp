9429318f79b4ae07fc4fab968c8b3d5ab064789e
Kalin <thegroup1516@gmail.com>
2017-05-01 12:51:23 +0300

    Adding the nedded things

--- BackEnd/DrawingApplication/app.js
+++ BackEnd/DrawingApplication/app.js
@@ -4,6 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
