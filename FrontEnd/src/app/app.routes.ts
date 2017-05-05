16ea0f688d378b415c8cf6902e8e3ffdacaf2c0b
Ftujio <svetlin954@abv.bg>
2017-05-05 14:17:30 +0300

    Added basic routing.

---
+++ FrontEnd/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { TimelineComponent } from './timeline/timeline.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '', component: TimelineComponent },
+];
+
+export const routing = RouterModule.forRoot(routes);
