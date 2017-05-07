fad36b013ead5a0a08140b7c617cf4875ce553d3
Ftujio <svetlin954@abv.bg>
2017-05-07 20:30:40 +0300

    Created the login form and the controls for it.
    * Need to make the validation.

--- FrontEnd/src/app/login/login.component.ts
+++ FrontEnd/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+	username = new FormControl('');
+	email = new FormControl('');
+	password = new FormControl('');
+	confirmPassword = new FormControl('');
 
-  constructor() { }
+	loginForm: FormGroup = this.builder.group({
+		username: this.username,
+		email: this.email,
+		password: this.password,
+		confirmPassword: this.confirmPassword
+	});
 
-  ngOnInit() {
-  }
+	constructor(private builder: FormBuilder) { }
+
+	ngOnInit() {
+	}
+
+	login(){
+		console.log(this.loginForm.value);
+	}
 
 }
