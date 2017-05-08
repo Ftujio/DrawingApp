3f90de29cc90e5b246c678f2f31a5d0df698a3c1
Ftujio <svetlin954@abv.bg>
2017-05-08 23:15:52 +0300

    Cleanded up some code and removed unused stuff.

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var seesion = require('express-session')
@@ -22,23 +21,17 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 var picture = require('./routes/picture');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.use('/', index);
+app.use('/users', users);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(seesion({secret:"abcdefghijklmnnmlkjihgfedcba", resave:false, saveUninitialized:true}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
-
-app.use('/', index);
-app.use('/users', users);
-app.use('/', picture);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
