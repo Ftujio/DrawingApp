ced23227fcb859a39a8433409ddf257da3d516a7
Ftujio <svetlin954@abv.bg>
2017-05-08 23:17:41 +0300

    Made a response for the index page and removed register and login logic.

--- routes/index.js
+++ routes/index.js
@@ -5,63 +5,7 @@ var User = require('../models/User');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-/* POST make registration */
-router.post('/register', function (req, res) {
-    var username = req.body.username;
-    var email = req.body.email;
-    var password = req.body.password;
-
-    var newuser = new User();
-    newuser.username = username;
-    newuser.email = email;
-    newuser.password = password;
-    newuser.save(function (err, savedUser) {
-        if (err) {
-            console.log(err);
-            return res.status(500).send();
-        }
-
-        return res.status(200).send();
-    })
-});
-
-/* POST the user can login */
-router.post('/login', function (req, res) {
-    var username = req.body.username;
-    var password = req.body.password;
-
-    User.findOne({username: username}, function (err, user) {
-        if (err) {
-            console.log(err);
-            return res.status(500).send();
-        }
-
-        if (!user){
-            console.log('There is no such a use');
-            return res.status(404).send();
-        }
-
-        user.comparePassword(password, function (err, isMatch) {
-            if (isMatch && isMatch == true) {
-              /* store the user in the session */
-                req.session.user = user;
-
-                return res.status(200).send();
-            } else {
-                console.log('The password is incorrect');
-                return res.status(401).send();
-            }
-        });
-    })
-});
-
-/* GET the user can logout */
-router.get('/logout', function (req, res) {
-    req.session.destroy();
-    return res.status(200).send();
+  res.send("INDEX");
 });
 
 module.exports = router;
