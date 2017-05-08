669e96803314342ad5a58d2c0f146d249ad1ab1f
Ftujio <svetlin954@abv.bg>
2017-05-08 23:53:50 +0300

    Renamed user model file to "user".

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var User = require('../models/User');
+var User = require('../models/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
