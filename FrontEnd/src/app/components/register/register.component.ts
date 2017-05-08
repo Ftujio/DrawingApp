612a7cfdacdf93118b9a3ab66d674e1c5e43b550
Ftujio <svetlin954@abv.bg>
2017-05-08 17:15:52 +0300

    Changed stuff.

--- FrontEnd/src/app/components/register/register.component.ts
+++ FrontEnd/src/app/components/register/register.component.ts
@@ -7,11 +7,28 @@ import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
 	styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+	// loginForm: FormGroup = this.builder.group({
+	// 	'username': [null, Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(10)])],
+	// 	'email': [null, Validators.required],
+	// 	'password': [null, Validators.required],
+	// 	'confirmPassword': [null, Validators.required],
+	// });
+
 	loginForm: FormGroup = this.builder.group({
-		'username': [null, Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(10)])],
-		'email': [null, Validators.required],
-		'password': [null, Validators.required],
-		'confirmPassword': [null, Validators.required],
+		username: new FormControl('', [
+			Validators.required,
+			Validators.minLength(5),
+			Validators.maxLength(10)
+		]),
+		email: new FormControl('', [
+			Validators.required
+		]),
+		password: new FormControl('', [
+			Validators.required
+		]),
+		confirmPassword: new FormControl('', [
+			Validators.required
+		]),
 	});
 
 	constructor(private builder: FormBuilder) { }
