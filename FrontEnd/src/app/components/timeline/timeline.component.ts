31ab56bda267c747e9ae568916854a995896026e
Ftujio <svetlin954@abv.bg>
2017-05-08 12:39:17 +0300

    .

---
+++ FrontEnd/src/app/components/timeline/timeline.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+import { PostService } from '../../services/post.service';
+
+@Component({
+	selector: 'app-timeline',
+	templateUrl: './timeline.component.html',
+	styleUrls: ['./timeline.component.css'],
+	providers: [PostService]
+})
+export class TimelineComponent implements OnInit {
+
+	data: Object;
+
+	constructor(posts: PostService) {
+		posts.getPosts().subscribe(data => {
+			this.data = data;
+			console.log("Data retrieved successfully");
+		})
+	}
+
+	ngOnInit() {
+	}
+
+}
