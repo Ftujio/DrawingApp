7702c6fdbfad71ad4d330cba91c87b481e7359dc
Ftujio <svetlin954@abv.bg>
2017-05-08 23:31:28 +0300

    Fix on the requests.

--- routes/users.js
+++ routes/users.js
@@ -2,21 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/register', (req, res, next) => {
+router.post('/register', (req, res, next) => {
   res.send('REGISTER');
 });
 
-router.get('/authenticate', (req, res, next) => {
+router.post('/authenticate', (req, res, next) => {
 	res.send('AUTHENTICATE');
 });
 
 router.get('/profile', (req, res, next) => {
 	res.send('PROFILE');
 });
-
-router.get('/validate', (req, res, next) => {
-	res.send('VALIDATE');
-});
-
 
 module.exports = router;
