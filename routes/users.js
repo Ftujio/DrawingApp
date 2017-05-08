df5f3c31c2112d50f9b6e7438971df1eb0802be4
Ftujio <svetlin954@abv.bg>
2017-05-08 23:12:09 +0300

    Basic routing.

--- routes/users.js
+++ routes/users.js
@@ -2,20 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/register', (req, res, next) => {
+  res.send('REGISTER');
 });
 
+router.get('/authenticate', (req, res, next) => {
+	res.send('AUTHENTICATE');
+});
 
-router.get('/dashboard', function (req, res) {
-   if (!req.session.user) { // check if the session has the user object
-        console.log('you are not log in');
-//     return res.status(401).send();
-   }
-
-   /* If the session exist return the ... */
-    console.log('log in');
-   //   return res.status(200).send("Welcome to super-secret API");
+router.get('/profile', (req, res, next) => {
+	res.send('AUTHENTICATE');
 });
 
 
