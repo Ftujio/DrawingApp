be2a10aea401207946233e16ea57da00bece9847
Ftujio <svetlin954@abv.bg>
2017-05-09 03:51:39 +0300

    User login through the angular form is done.

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
 	selector: 'app-login',
@@ -16,13 +20,33 @@ export class LoginComponent implements OnInit {
 		]),
 	});
 
-	constructor(private builder: FormBuilder) { }
+	constructor(
+		private builder: FormBuilder,
+		private authService: AuthService,
+		private router: Router,
+		private _flashMessagesService: FlashMessagesService
+	) { }
 
 	ngOnInit() {
 	}
 
 	login(){
-		console.log(this.loginForm.valid);
+		if(this.loginForm.valid){
+			const user = {
+				username: this.loginForm.value.username,
+				password: this.loginForm.value.password
+			}
+
+			this.authService.authenticateUser(user).subscribe(data => {
+				if(data.success){
+					this.authService.storeUserData(data.token, data.user);
+					this.router.navigate(['/profile']);
+				} else {
+					//this._flashMessagesService.show(data.msg, {cssClass: 'alert-danger', timeout: 5000});
+					this.router.navigate(['/login']);
+				}
+			});
+		}
 	}
 
 }
