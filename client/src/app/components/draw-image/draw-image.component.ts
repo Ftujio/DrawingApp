a3288ef5895d94c034f54f7c73ac45ce3927ab40
Ftujio <svetlin954@abv.bg>
2017-05-09 05:22:22 +0300

    Integrated the drawing app layout into the website.

--- client/src/app/components/draw-image/draw-image.component.ts
+++ client/src/app/components/draw-image/draw-image.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-draw-image',
   templateUrl: './draw-image.component.html',
-  styleUrls: ['./draw-image.component.css']
+  styleUrls: ['./draw-image.component.css', './icons.css', './main.css', './reset.css', './side-toolbar.css', './toolbar.css']
 })
 export class DrawImageComponent implements OnInit {
 
