df148f2857264c2dbccfd604ab1a137e9ba64b16
Ftujio <svetlin954@abv.bg>
2017-05-15 12:37:54 +0300

    Now the side toolbar shows which color is seleced.

--- public2/js/side-toolbar.js
+++ public2/js/side-toolbar.js
@@ -1,9 +1,9 @@
 let element = document.getElementsByClassName("color");
 var color = "black";
 
-setCurrentTool();
+setToolBGColor2();
 
-function setToolBGColor(){
+function setToolBGColor2(){
 	for(let i = 0; i < toolNum - 1; i++){
 		element[i].parentNode.style.backgroundColor = "#f2f2f2";
 	}
@@ -13,6 +13,7 @@ for(let i = 0; i < element.length; i++){
 	element[i].style.backgroundColor = element[i].id;
 	element[i].addEventListener("click", function(e){
 		color = element[i].id;
+		setToolBGColor2();
 		element[i].parentNode.style.backgroundColor = "#d9d9d9";
 	});
 }
