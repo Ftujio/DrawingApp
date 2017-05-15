a086a4d5f85906d29675e2a41eab31dff4fc9c44
Ftujio <svetlin954@abv.bg>
2017-05-15 12:51:02 +0300

    Fixed bug with the background color and the black is selected by default.

--- public2/js/side-toolbar.js
+++ public2/js/side-toolbar.js
@@ -3,8 +3,15 @@ var color = "black";
 
 setToolBGColor2();
 
+for(let i = 0; i < toolNum; i++){
+	element[i].parentNode.style.backgroundColor = "#f2f2f2";
+	if(i == (toolNum - 1)){
+		element[i + 1].parentNode.style.backgroundColor = "#d9d9d9";
+	}
+}
+
 function setToolBGColor2(){
-	for(let i = 0; i < toolNum - 1; i++){
+	for(let i = 0; i < element.length; i++){
 		element[i].parentNode.style.backgroundColor = "#f2f2f2";
 	}
 }
@@ -13,6 +20,7 @@ for(let i = 0; i < element.length; i++){
 	element[i].style.backgroundColor = element[i].id;
 	element[i].addEventListener("click", function(e){
 		color = element[i].id;
+		console.log(color);
 		setToolBGColor2();
 		element[i].parentNode.style.backgroundColor = "#d9d9d9";
 	});
